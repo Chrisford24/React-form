98242e39c4986ef396f7c807296b2488e5f56c8a
Christopher Ford <Christopher11ford@yahoo.com>
2019-05-21 15:26:14 -0500

    finished

---
+++ src/components/UserForms.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
+import FormPersonalDetails from './FormPersonalDetails';
+import Confirm from './Confirm';
+import Success from './Success';
+
+export class UserForms extends Component {
+  // Properties
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    occupation: '',
+    city: '',
+    bio: ''
+  };
+
+  // Methods
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  // Handle change in the fields
+
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  render() {
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupation, city, bio };
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+            prevStep={this.prevStep}
+          />
+        );
+      case 3:
+        return (
+          <Confirm
+            nextStep={this.nextStep}
+            values={values}
+            prevStep={this.prevStep}
+          />
+        );
+      case 4:
+        return <Success />;
+    }
+  }
+}
+
+export default UserForms;
